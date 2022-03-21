6c9ddf358cba35b82dcfd42dfde749cbd7c6d031
constantinp2022 <pricopecostel98@yahoo.com>
2022-03-21 08:55:22 +0000

    Week3: move Energy

--- circuits/moveEnergy/circuit_cpp/circuit.cpp
+++ circuits/moveEnergy/circuit_cpp/circuit.cpp
@@ -377,13 +377,13 @@ Fr_copy(aux_dest,&circuitConstants[3]);
 PFrElement aux_dest = &lvar[1];
 // load src
 // end load src
-Fr_copy(aux_dest,&circuitConstants[5]);
+Fr_copy(aux_dest,&circuitConstants[7]);
 }
 {
 PFrElement aux_dest = &lvar[2];
 // load src
 // end load src
-Fr_copy(aux_dest,&circuitConstants[7]);
+Fr_copy(aux_dest,&circuitConstants[5]);
 }
 {
 uint aux_create = 0;
